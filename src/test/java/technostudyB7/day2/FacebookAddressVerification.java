be079476941953f750f8a4a1860d81accff2006f
busrav <busravural67@gmail.com>
2023-02-07 20:31:37 -0500

    add

---
+++ src/test/java/technostudyB7/day2/FacebookAddressVerification.java
@@ -0,0 +1,16 @@
+package technostudyB7.day2;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FacebookAddressVerification {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver= new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("https://www.facebook.com") ;
+    }
+
+}
