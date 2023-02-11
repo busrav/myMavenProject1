398358afd3938a202dfb40861216d9d12d6c5e28
busrav <busravural67@gmail.com>
2023-02-11 14:38:36 -0500

    corrected

--- src/test/java/technostudyB7/seleniumTasks/TC2.java
+++ src/test/java/technostudyB7/seleniumTasks/TC2.java
@@ -25,17 +25,28 @@ Expected: “Log into Facebook”*/
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
-        String email = "busravural67@gmail.com";
+        String email = "bus4546@gmail.com";
         String password = "12345";
 
         driver.get("https://www.facebook.com");
         WebElement emailBox = driver.findElement(By.id("email"));
-        emailBox.sendKeys("abc@gmail.com");
+        emailBox.sendKeys(email);
 
         WebElement passwordBox = driver.findElement(By.cssSelector("input[type='password']"));
-        passwordBox.sendKeys("8888");
+        passwordBox.sendKeys(password);
 
         WebElement loginButton = driver.findElement(By.cssSelector("button[name='login']"));
         loginButton.click();
+
+        String newTitle = driver.getTitle();
+        String expTitle = "Log into Facebook";
+
+        if(newTitle.equals(expTitle)){
+            System.out.println("verification: pass");
+            System.out.println("incorrect username and incorrect password");
+        }else{
+            System.out.println("verification: failed");
+            System.out.println("actual title is: " + newTitle);
+        }
     }
 }
