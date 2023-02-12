d8eb7be9df66ca7da79486a82fd7673062f27199
busrav <busravural67@gmail.com>
2023-02-11 21:11:01 -0500

    corrected

--- src/test/java/technostudyB7/seleniumTasks/TC10.java
+++ src/test/java/technostudyB7/seleniumTasks/TC10.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.concurrent.TimeUnit;
+
 public class TC10 {
     /*TC #9: EtsySearchTest
 1- Navigate to etsy homepage
@@ -23,6 +25,8 @@ https://www.etsy.com/
         driver.manage().window().maximize();
         driver.navigate().to("https://www.etsy.com/");
 
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
 //        <input id="global-enhancements-search-query" data-id="search-query" data-search-input="" type="text" name="search_query"
 //        class="wt-input wt-input-btn-group__input global-enhancements-search-input-btn-group__input
 //                     wt-pr-xs-7
@@ -33,12 +37,27 @@ https://www.etsy.com/
         search.sendKeys(input + Keys.ENTER);
         String actTitle = driver.getTitle();
         String expTitle = "part of the item";
+
+        if (actTitle.equals(expTitle)){
+            System.out.println("pass");
+        }else{
+            System.out.println("failed");
+            System.out.println(actTitle);
+        }
 //        <span class="wt-display-table-cell wt-text-left-xs">Gifts for Every Valentine</span>
-        Thread.sleep(3000);
+
         WebElement link = driver.findElement(By.cssSelector("span[class='wt-display-table-cell wt-text-left-xs']"));
         link.click();
 //        <h1 class="wt-text-display-01 wt-pb-xs-2 wt-text-black">Valentine's Day Gifts</h1>
         WebElement header = driver.findElement(By.cssSelector("h1[class='wt-text-display-01 wt-pb-xs-2 wt-text-black']"));
-        header.getText();
+        String actHeader = header.getText();
+        String expHeader = "Gifts for Every Valentine";
+
+        if (actHeader.equals(expHeader)){
+            System.out.println("pass");
+        }else{
+            System.out.println("failed");
+            System.out.println(actHeader);
+        }
     }
 }
